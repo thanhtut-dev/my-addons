c7f2e804a6b78df12841bd7f73be47a356253643
Than Htut <saithanhtut.ml@gmail.com>
2025-12-04 23:03:58 +0700

    Add conditions in partner_ids fields in lead and order

--- customer_access_by_sales_team/models/crm_lead.py
+++ customer_access_by_sales_team/models/crm_lead.py
@@ -7,8 +7,11 @@ class CrmLead(models.Model):
     @api.depends('user_id')
     def _compute_partner_ids(self):
         for lead in self:
-            teams = self.env['crm.team'].search([
-                ('company_id', 'in', (False, self.env.company.id)),
-                '|', ('user_id', '=', lead.user_id.id), ('member_ids', 'in', [lead.user_id.id])
-            ])
-            lead.partner_ids = teams.partner_ids
+            if self.env.user.has_group('sales_team.group_sale_manager'):
+                lead.partner_ids = self.env['res.partner'].search([('customer_rank', '>', 0)])
+            else:
+                teams = self.env['crm.team'].search([
+                    ('company_id', 'in', (False, self.env.company.id)),
+                    '|', ('user_id', '=', lead.user_id.id), ('member_ids', 'in', [lead.user_id.id])
+                ])
+                lead.partner_ids = teams.partner_ids
