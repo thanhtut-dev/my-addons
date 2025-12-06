171b708400435875b4cca2638d3b05871183d5ea
Than Htut <saithanhtut.ml@gmail.com>
2025-12-06 23:02:13 +0700

    Initial commit for version 17

---
+++ product_access_by_sales_team/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Product Access by Sales Team',
+    'version': '17.0',
+    'category': 'Sales',
+    'summary': 'Restrict product visibility by Sales Team. Allows each team to access and sell only products from'
+               ' assigned categories, including filtered quotation templates and product variants.',
+    'description': 'static/description/index.html',
+    'images': ['static/description/thumbnail.png'],
+    'author': 'thanhtut-dev',
+    'website': 'https://thanhtut-dev.odoo.com/',
+    'license': 'LGPL-3',
+    'depends': ['account','crm','sale','sales_team','sale_management','web'],
+    'data': [
+            'views/crm_team_views.xml',
+            'views/sale_order_views.xml',
+            'views/sale_order_template_view.xml',
+        ],
+    'installable': True,
+    'application': True,
+}
